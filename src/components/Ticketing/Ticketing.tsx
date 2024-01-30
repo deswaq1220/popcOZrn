32775c1ed984353dc62fe14e48cb23a8583c49b1
chaayee0123 <chayee0614@gmail.com>
2024-01-31 03:40:57 +0900

    ticketing-right.css

--- src/components/Ticketing/Ticketing.tsx
+++ src/components/Ticketing/Ticketing.tsx
@@ -34,7 +34,107 @@ const Ticketing = () => {
         <img className="posters" src="/src/img/poster1.avif"></img>
       </div>
       <div className="verticle-line"></div>
-    </div>
+    
+    <div className="right-container">
+      <div className="screen">screen</div>
+      <div className="seatsContainer">
+        <div className="row1">
+          <span className="seatA">A</span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+        </div>
+        <div className="row">
+          <span className="seatB">B</span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="occupiedSeat"></span>
+          <span className="occupiedSeat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+        </div>
+        <div className="row">
+          <span className="seatC">C</span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="occupiedSeat"></span>
+          <span className="occupiedSeat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+        </div>
+        <div className="row">
+          <span className="seatD">D</span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+        </div>
+        <div className="row">
+          <span className="seatE">E</span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+        </div>
+        <div className="row">
+          <span className="seatF">F</span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="occupiedSeat"></span>
+          <span className="occupiedSeat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+          <span className="seat"></span>
+        </div>
+      </div>
+
+      <div className="showcase">
+        <li>
+          <div className="availableSeatIcon"></div>
+          <div className="availableSeat-text">선택가능</div>
+        </li>
+        <li>
+          <div className="selectedSeatIcon"></div>
+          <div className="selectedSeat-text">선택좌석</div>
+        </li>
+        <li>
+          <div className="occupiedSeatIcon"></div>
+          <div className="occupiedSeat-text">예매완료</div>
+        </li>
+      </div>
+      <div className="sum-price">총 합계: </div>
+      </div>
+  </div>
   );
 };
 
