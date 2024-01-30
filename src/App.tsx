ccff59bee225e8e482e632b4d377834160e67bc0
hayeon010904 <hayeon010904@gmail.com>
2024-01-31 02:30:10 +0900

    ticketing-left.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,23 @@
-import { useState } from "react";
 import Nav from "./components/Nav/Nav";
-
 import { Routes, Route, Link, Outlet } from "react-router-dom";
 import Ticketing from "./components/Ticketing/Ticketing";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import { useHistory } from "react-router-dom";
-// console.log(auth);
-
-const App: React.FC = () => {
-  const Layout = () => {
-    return (
-      <>
-        <Nav />
-        <Outlet />
-      </>
-    );
-  };
-
+const Layout = () => {
   return (
-    <div>
+    <>
+      <Nav />
+      <Outlet />
+    </>
+  );
+};
+const App: React.FC = () => {
+  return (
+    <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/ticketing" element={<Ticketing />} />
         </Route>
       </Routes>
-    </div>
+    </>
   );
 };
 
--- src/components/Ticketing/Ticketing.tsx
+++ src/components/Ticketing/Ticketing.tsx
@@ -1,24 +1,40 @@
-import React from "react";
-// import './Ticketing.css';
-import SelectBox from "./SelectBox";
-import Count from "./Count";
 import "./Ticketing.css";
+const SelectBox = () => {
+  return (
+    <select className="select">
+      <option key="movie1" value="movie1">
+        movie1
+      </option>
+      <option key="movie2" value="movie2">
+        movie2
+      </option>
+      <option key="movie3" value="movie3">
+        movie3
+      </option>
+      <option key="movie4" value="movie4">
+        movie4
+      </option>
+    </select>
+  );
+};
 
 const Ticketing = () => {
   return (
-    <>
-      <form className="movie_select">
-        <li>
-          <label className="select" />
-          영화선택
-          <SelectBox />
-        </li>
-        <li>
-          <label className="count" />총 인원
-        </li>
-      </form>
-      <section></section>
-    </>
+    <div className="ticketing-container">
+      <div className="left-container">
+        <div className="option">
+          <div className="select-box">
+            영화선택 <SelectBox />
+          </div>
+          <div className="count-people">
+            총 인원
+            <p className="count"></p>
+          </div>
+        </div>
+        <img className="posters" src="/src/img/poster1.avif"></img>
+      </div>
+      <div className="verticle-line"></div>
+    </div>
   );
 };
 
