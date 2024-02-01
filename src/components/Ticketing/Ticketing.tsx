c89ef6c0558d1b857a22f418bb79db499cea4bc6
hayeon <hayeon010904@gmail.com>
2024-02-01 21:30:50 +0900

    select컴포넌트 분리

--- src/components/Ticketing/Ticketing.tsx
+++ src/components/Ticketing/Ticketing.tsx
@@ -1,63 +1,59 @@
 import "./Ticketing.css";
 import Seat from "./Seat";
 import { useState } from "react";
-interface Movie {
+import Select from "./Select";
+export interface Movie {
   title: string;
-  poster: JSX.Element;
+  poster: string;
 }
 const Ticketing: React.FC = () => {
   const movies: Movie[] = [
-    { title: "웡카", poster: <img src="/src/img/posters/웡카.jpeg"></img> },
+    { title: "웡카", poster: "/src/img/posters/웡카.jpeg" },
     {
       title: "시민덕희",
-      poster: <img src="/src/img/posters/시민덕희.jpeg"></img>,
+      poster: "/src/img/posters/시민덕희.jpeg",
     },
     {
       title: "외계인+2부",
-      poster: <img src="/src/img/posters/외계인.jpeg"></img>,
+      poster: "/src/img/posters/외계인.jpeg",
     },
-    { title: "소풍", poster: <img src="/src/img/posters/소풍.jpeg"></img> },
+    { title: "소풍", poster: "/src/img/posters/소풍.jpeg" },
     {
       title: "도그데이즈",
-      poster: <img src="/src/img/posters/도그데이즈.jpeg"></img>,
+      poster: "/src/img/posters/도그데이즈.jpeg",
     },
   ];
   console.log(movies);
-  const [poster, setPoster] = useState<JSX.Element>(movies[0].poster);
-
-  const handlePoster = (e) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const handlePoster = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedTitle = e.target.value;
     const selectedIndex = movies.findIndex(
       (movie) => movie.title === selectedTitle
     );
-
-    setPoster(movies[selectedIndex].poster);
-    console.log(selectedIndex);
+    setSelectedIndex(selectedIndex);
   };
-
   return (
     <div className="ticketing-container">
       <div className="left-container">
         <div className="option">
           <div className="select-box">
             영화선택{" "}
-            <select onChange={handlePoster} className="select">
-              {movies.map((movie, index) => (
-                <option key={index}>{movie.title}</option>
-              ))}
-            </select>
+            <Select
+              movies={movies}
+              selectedIndex={selectedIndex}
+              onSelectItem={handlePoster}
+            />
           </div>
           <div className="count-people">
             총 인원
             <p className="count"></p>
           </div>
         </div>
-        <img className="posters" src={poster.props.src}></img>
+        <img className="posters" src={movies[selectedIndex].poster} />
       </div>
       <div className="verticle-line"></div>
       <Seat />
     </div>
   );
 };
-
 export default Ticketing;
